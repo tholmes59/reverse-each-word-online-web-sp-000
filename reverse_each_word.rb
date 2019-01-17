0545e93cfbd2ca379c719852822c2745b370bd30
Tom Holmes <tholmes59@gmail.com>
2019-01-17 00:02:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
   array = string.split(/ /)
+  new_array = []
   array.each do |word|
-    word.reverse 
+    new_array << word.reverse 
   end
+  new_array.join(" ")
 end
