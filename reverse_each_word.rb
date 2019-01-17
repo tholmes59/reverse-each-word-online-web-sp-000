2fad95869a385cfe9f5bc8cff3d556e142bb49da
Tom Holmes <tholmes59@gmail.com>
2019-01-17 00:04:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,11 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(/ /)
+  array.collect do |word|
+   word.reverse 
+  end
+  array.join(" ")
+end
